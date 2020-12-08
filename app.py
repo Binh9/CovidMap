304dfa334951a83c23bd91f126d2dd0ed64d4dae
Binh <lebinhb98@gmail.com>
2020-12-08 03:18:57 -0500

    sections

--- app.py
+++ app.py
@@ -8,14 +8,37 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+# -------------------------------------------------------------------------------------------------
+# LALALA
+
+# -------------------------------------------------------------------------------------------------
+# APP LAYOUT
 app.layout = html.Div(children = [
     html.H1(children = "DASH APP"),
 
     html.Div(children = '''
         Dash: A web application framework for Python
     '''),
+
+    html.Div(children = '''
+        PLACEHOLDER FOR GENERAL STAT
+    '''),
+
+    html.Div(children = '''
+        PLACEHOLDER FOR MAP
+    '''),
+
+    html.Div(children = '''
+        PLACEHOLDER FOR ADDITIONAL VISUALS
+    '''),
 ])
 
+# -------------------------------------------------------------------------------------------------
+# Dash Components
 
+
+
+# -------------------------------------------------------------------------------------------------
+# Running point
 if __name__ == '__main__':
     app.run_server(debug = True)
