ecbc5fad61c7862e3eb8439c0fa6599c40744a10
Binh <lebinhb98@gmail.com>
2020-12-08 03:01:05 -0500

    running server

--- app.py
+++ app.py
@@ -5,3 +5,17 @@ import plotly.express as px
 import pandas as pd
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(children = [
+    html.H1(children = "DASH APP"),
+
+    html.Div(children = '''
+        Dash: A web application framework for Python
+    '''),
+])
+
+
+if __name__ == '__main__':
+    app.run_server(debug = True)
